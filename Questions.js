6c64eb9bed59d00205d57f1a5323184dd84f20d4
Elise Bal <m11elbal@gmail.com>
2020-06-28 16:21:31 -0500

    can see correct answers

--- Questions.js
+++ Questions.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter, NavLink } from 'react-router-dom';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TouchableHighlight,
+} from 'react-native';
 import { Button } from 'react-native-elements';
+import { greaterThan } from 'react-native-reanimated';
 // import ScoreModal from './Modal';
 
 export class Questions extends React.Component {
@@ -10,38 +17,50 @@ export class Questions extends React.Component {
     super();
     this.state = {
       numberCorrect: 0,
+      correct: false,
+      incorrect: false,
+      correctIds: [],
     };
     this.handleChoice = this.handleChoice.bind(this);
   }
 
-  handleChoice(choice, result, buttonId) {
-    let button = document.getElementById(buttonId);
+  handleChoice(choice, result, id) {
+    // let button = document.getElementById(buttonId);
 
-    if (button && choice === result.correct_answer) {
+    if (id && choice === result.correct_answer) {
       this.setState({ numberCorrect: this.state.numberCorrect + 1 });
-      button.style.backgroundColor = 'green';
-      button.style.color = 'white';
-      button.disabled = true;
+      this.setState({
+        correct: true,
+      });
+
+      this.setState({ correctIds: [...this.state.correctIds, id] });
+
+      //   button.style.backgroundColor = 'green';
+      //   button.style.color = 'white';
+      //   button.disabled = true;
     } else {
-      button.style.backgroundColor = 'red';
+      this.setState({ incorrect: true });
     }
-    for (let i = 0; i < 4; i++) {
-      let id = buttonId[0] + '-' + i;
-      let element = document.getElementById(id);
+    //  else {
+    //   button.style.backgroundColor = 'red';
+    // }
+    // for (let i = 0; i < 4; i++) {
+    //   let id = buttonId[0] + '-' + i;
+    //   let element = document.getElementById(id);
 
-      this.markCorrectAnswer(element, result.correct_answer);
-      element.disabled = true;
-    }
+    //   this.markCorrectAnswer(element, result.correct_answer);
+    //   element.disabled = true;
+    // }
   }
 
-  markCorrectAnswer(element, correctAnswer) {
-    if (
-      element.innerHTML === correctAnswer ||
-      element.getAttribute('choice') === correctAnswer
-    ) {
-      element.style.backgroundColor = 'green';
-    }
-  }
+  //   markCorrectAnswer(element, correctAnswer) {
+  //     if (
+  //       element.innerHTML === correctAnswer ||
+  //       element.getAttribute('choice') === correctAnswer
+  //     ) {
+  //       element.style.backgroundColor = 'green';
+  //     }
+  //   }
 
   render() {
     const results = this.props.results.results;
@@ -71,18 +90,16 @@ export class Questions extends React.Component {
                 <View style={styles.choiceForm}>
                   {result.shuffledAnswers.map((choice, buttonIndex) => (
                     <View key={choice.incorrect_answers}>
-                      <Button
-                        disabled={false}
-                        style={styles.choice}
-                        title={choice
-                          .replace(/&quot;/g, '"')
-                          .replace(/&#039;/g, "'")
-                          .replace(/&lt;/g, '<')
-                          .replace(/&gt;/g, '>')
-                          .replace(/&lrm;/g, '')
-                          .replace(/&oacute;/g, 'ó')}
-                        // dangerouslySetInnerHTML={{ __html: choice }}
-                        id={questionIndex + '-' + buttonIndex}
+                      <TouchableHighlight
+                        activeOpacity={1}
+                        style={
+                          this.state.correctIds.includes(
+                            questionIndex + '-' + buttonIndex
+                          )
+                            ? styles.correct
+                            : styles.choice
+                        }
+                        // id={questionIndex + '-' + buttonIndex}
                         onPress={() => {
                           this.handleChoice(
                             choice,
@@ -90,7 +107,17 @@ export class Questions extends React.Component {
                             questionIndex + '-' + buttonIndex
                           );
                         }}
-                      />
+                      >
+                        <Text>
+                          {choice
+                            .replace(/&quot;/g, '"')
+                            .replace(/&#039;/g, "'")
+                            .replace(/&lt;/g, '<')
+                            .replace(/&gt;/g, '>')
+                            .replace(/&lrm;/g, '')
+                            .replace(/&oacute;/g, 'ó')}
+                        </Text>
+                      </TouchableHighlight>
                     </View>
                   ))}
                 </View>
@@ -135,5 +162,15 @@ const styles = StyleSheet.create({
   choice: {
     width: 250,
     margin: 3,
+    backgroundColor: 'pink',
+  },
+  correct: {
+    backgroundColor: 'green',
+    color: 'white',
+    width: 250,
+    margin: 3,
+  },
+  incorrect: {
+    backgroundColor: 'red',
   },
 });
